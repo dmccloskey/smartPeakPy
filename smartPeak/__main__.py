0215aa890f161d580811a09ad414145981dd1dc7
Douglas McCloskey <dmccloskey87@gmail.com>
2017-03-07 09:52:30 +0100

    added cmd parameter reader

--- smartPeak/__main__.py
+++ smartPeak/__main__.py
@@ -4,7 +4,7 @@ from .smartPeak_openSWATH_cmd import smartPeak_openSWATH_cmd
 class __main__():
     def run_openSWATH_cmd(
             self,
-            filename='C:/Users/domccl/"Dropbox (UCSD SBRG)"/Project_FastPeak/openMS_MRMworkflow/openSWATH_cmd_params.csv',
+            filename='C:/Users/domccl/Dropbox (UCSD SBRG)/Project_FastPeak/openMS_MRMworkflow/openSWATH_cmd_params.csv',
             verbose=True):
         """ """
         openSWATH_cmd = smartPeak_openSWATH_cmd()
--- smartPeak/smartPeak_openSWATH_cmd.py
+++ smartPeak/smartPeak_openSWATH_cmd.py
@@ -1,16 +1,17 @@
 # coding: utf-8
 #system
-import os, csv, sys
+import csv, sys
 #module
 from .smartPeak import smartPeak
 
+
 class smartPeak_openSWATH_cmd():
-    def __init__(self,openSWATH_cmd_params_I=None):
+    def __init__(self, openSWATH_cmd_params_I=None):
 
         if openSWATH_cmd_params_I and not openSWATH_cmd_params_I is None:
-            self.openSWATH_cmd_params = openSWATH_cmd_params_I;
+            self.openSWATH_cmd_params = openSWATH_cmd_params_I
         else:
-            self.openSWATH_cmd_params_I = None;
+            self.openSWATH_cmd_params_I = None
 
         # self.openSWATH_cmd_params = [
         #     #file 1
@@ -66,7 +67,7 @@ class smartPeak_openSWATH_cmd():
 
         # ]
 
-    def openSWATH_cmd(self,verbose_I=False):
+    def openSWATH_cmd(self, verbose_I=False):
         """openSWATH command line workflow
         
         FUNCTION ORDER:
@@ -80,11 +81,11 @@ class smartPeak_openSWATH_cmd():
         OpenSwathConfidenceScoring: score the picked peaks
         OpenSwathFeatureXMLToTSV: convert to csv
         """
-        smartpeak = smartPeak();
+        smartpeak = smartPeak()
         for line in self.openSWATH_cmd_params:
-            for fnc,params in line.items():
-                cmd = smartpeak.make_osCmd(params,fnc)
-                smartpeak.run_osCmd(cmd,verbose_I=verbose_I)
+            for fnc, params in line.items():
+                cmd = smartpeak.make_osCmd(params, fnc)
+                smartpeak.run_osCmd(cmd, verbose_I=verbose_I)
 
     def read_csv(self, filename):
         """read table data from csv file
@@ -96,53 +97,76 @@ class smartPeak_openSWATH_cmd():
             data (list): list of table data
 
         """
-        data = [];
+        data = []
         try:
             with open(filename, 'r') as csvfile:
-                reader = csv.DictReader(csvfile);
+                reader = csv.DictReader(csvfile)
                 try:
-                    keys = reader.fieldnames;
+                    keys = reader.fieldnames
                     for row in reader:
-                        self.data.append(row);
+                        data.append(row)
                 except csv.Error as e:
-                    sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e));
+                    sys.exit('file %s, line %d: %s' %
+                             (filename, reader.line_num, e))
         except IOError as e:
-            sys.exit('%s does not exist' % e);
+            sys.exit('%s does not exist' % e)
         return data
 
-    def parse_openSWATH_cmd_params(self,data_I):
+    def parse_openSWATH_cmd_params(self, data_I):
         """parse parameters from csv file
-        
+
         Args
-            data_I (list)
+            data_I (list): e.g. [
+                {'function': 'ConvertTSVToTraML', 'name': '-in', 'delim': ' ', 'value': 'IsolateA1.csv'},
+                {'function': 'ConvertTSVToTraML', 'name': '-out', 'delim': ' ', 'value': 'IsolateA1.traML'},
+                {'function': 'MRMMapper', 'name': '-in', 'delim': ' ', 'value': 'IsolateA1.mzML'},
+                {'function': 'MRMMapper', 'name': '-tr', 'delim': ' ', 'value': 'IsolateA1.traML'},
+                {'function': 'MRMMapper', 'name': '-out', 'delim': ' ', 'value': 'IsolateA1.csv'},
+                {'function': 'MRMMapper', 'name': '-precursor_tolerance','delim':' ','value':0.5},
+                {'function': 'MRMMapper', 'name': '-product_tolerance','delim':' ','value':0.5},
+                {'function': 'MRMMapper', 'name': '-no-strict','delim':' ','value':''},
+            ]
 
         Returns
-            data_O (list)
+            data_O (list): e.g. [
+                {'ConvertTSVToTraML':[
+                    {'param':'-in','delim':' ','value':'IsolateA1.csv'},
+                    {'param':'-out','delim':' ','value':'IsolateA1.traML'}
+                ]},
+                {'MRMMapper':[
+                    {'param':'-in','delim':' ','value':'IsolateA1.mzML'},
+                    {'param':'-tr','delim':' ','value':'IsolateA1.traML'},
+                    {'param':'-out','delim':' ','value':'IsolateA1_features.mzML'},
+                    {'param':'-precursor_tolerance','delim':' ','value':0.5},
+                    {'param':'-product_tolerance','delim':' ','value':0.5},
+                    {'param':'-no-strict','delim':' ','value':''}
+                ]}
+                ]
         """
-        data_O = [];
-        function_current = '';
-        function_params = [];
+        data_O = []
+        function_current = ''
+        function_params = {}
         function_param = {}
-        for i,d in enumerate(data_I):
+        for i, d in enumerate(data_I):
             #skip non-used lines
-            if not d['used_'] or d['used_']=="FALSE":
-                continue;
+            if not d['used_'] or d['used_'] == "FALSE":
+                continue
             #update function_current
-            if d['function']!=function_current:
-                function_current=d['function'];
-                if function_params: #append only if list is not empty
+            if d['function'] != function_current:
+                function_current = d['function']
+                if function_params:  #append only if list is not empty
                     data_O.append(function_params)
-                function_params = [];
-                function_param = {};
+                function_params = {function_current:[]}
             #make the function parameter line
-            function_param['param']=d['name']
-            function_param['delim']=d['delim']
-            function_param['value']=d['value']
-            function_params.append(function_param)
+            function_param = {}
+            function_param['param'] = d['name']
+            function_param['delim'] = d['delim']
+            function_param['value'] = d['value']
+            function_params[function_current].append(function_param)
             #add in the last value
-            if i==len(data_I-1):
+            if i==len(data_I)-1:
                 data_O.append(function_params)
-        return data_O;            
+        return data_O
 
     def read_openSWATH_cmd_params(self, filename):
         """read table data from csv file representing
@@ -164,4 +188,4 @@ class smartPeak_openSWATH_cmd():
         """
         data_csv = self.read_csv(filename)
         data_params = self.parse_openSWATH_cmd_params(data_csv)
-        self.openSWATH_cmd_params = data_params;
+        self.openSWATH_cmd_params = data_params
