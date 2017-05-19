53d4ed228d10656889ab0f3bb437acd59f8aba39
Douglas McCloskey <dmccloskey87@gmail.com>
2017-05-19 12:12:44 +0200

    updates to dockerfile

--- cpp/docker-entrypoint.sh
+++ cpp/docker-entrypoint.sh
@@ -3,8 +3,8 @@ cd /home/user/code
 g++ -g main.cpp
 # gdb --interpreter=mi a.out
 ##launch the ssh server
-/usr/sbin/sshd -D
+# /usr/sbin/sshd -D
 ##manually keep the container running
 # sleep infinity
 ##launch the gbdserver
-# gdbserver :22 a.out
+gdbserver :3000 a.out
