5a282630e6cd34aef5c2df4dc23b717404981f4d
Douglas McCloskey <dmccloskey87@gmail.com>
2017-05-19 14:22:28 +0200

    updates to smartPeak_py

--- py/smartPeak/__main__.py
+++ py/smartPeak/__main__.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 from .smartPeak_openSWATH_cmd import smartPeak_openSWATH_cmd
 from .smartPeak_openSWATH_py import smartPeak_openSWATH_py
+from .smartPeak import smartPeak
 from .smartPeak_i import smartPeak_i
+from .smartPeak_o import smartPeak_o
 
 class __main__():
     def run_openSWATH_cmd(
@@ -58,3 +60,41 @@ class __main__():
         from .test_smartPeak import test_smartPeak
         tests = test_smartPeak()
         tests.test_parseString()
+
+    def convert_MQQMethod2Feature(filename_I,filename_O):
+        """Convert MultiQuant QMethod file to feature.csv file
+        
+        """
+        smartpeak_i = smartPeak_i()
+        smartpeak_i.read_csv(filename_I,delimiter=',')
+        MQQMethod = smartpeak_i.getData()
+        smartpeak_i.clear_data()
+
+        smartpeak = smartPeak()
+        features = smartpeak.convert_MQQMethod2Feature(MQQMethod)
+        headers = ['ProteinName',
+            'FullPeptideName',
+            'transition_group_id',
+            'transition_name',
+            'Tr_recalibrated',
+            'Annotation',
+            'RetentionTime',
+            'PrecursorMz',
+            'MS1 Res',
+            'ProductMz',
+            'MS2 Res',
+            'Dwell',
+            'Fragmentor',
+            'Collision Energy',
+            'Cell Accelerator Voltage',
+            'LibraryIntensity',
+            'decoy',
+            'PeptideSequence',
+            'LabelType',
+            'PrecursorCharge',
+            'FragmentCharge',
+            'FragmentType',
+            'FragmentSeriesNumber'
+        ]
+        smartpeak_o = smartPeak_o(features)
+        smartpeak_o.write_dict2csv(filename_O,headers=headers)
--- py/smartPeak/smartPeak.py
+++ py/smartPeak/smartPeak.py
@@ -35,3 +35,73 @@ class smartPeak():
         """
         if verbose_I: print(cmd_I)
         os.system("%s" % (cmd_I))
+
+    @staticmethod
+    def convert_MQQMethod2Feature(MQQMethod_I):
+        """Convert MultiQuant QMethod.csv file to feature.csv file
+        
+        Args
+            MQQMethod_I (list:dict): list of dictionaries of headers/values
+            
+        Returns
+            FeatureXML_O (list:dict): list of dictionaries of headers/values
+        
+        """
+        FeatureXML_O = []
+        #create header map:
+        header_map = {
+            'Group Name':'ProteinName',
+            'Group Name':'transition_group_id',
+            'Name':'transition_name',
+            'Expected RT':'Tr_recalibrated',
+            'Expected RT':'RetentionTime',
+            'Q1 Mass - 1':'PrecursorMz',
+            'Q3 Mass - 1':'ProductMz'
+        }
+        #create defaults:
+        feature_defaults = {
+            'FullPeptideName':'',
+            'Annotation':'',
+            'MS1 Res':'Unit',
+            'MS2 Res':'Unit',
+            'Dwell':'',
+            'Fragmentor':'',
+            'Collision Energy':'',
+            'Cell Accelerator Voltage':'',
+            'LibraryIntensity':'1',
+            'decoy':'0',
+            'PeptideSequence':'',
+            'PrecursorCharge':'1',
+            'FragmentCharge':'1',
+            'FragmentType':''
+        }
+        for row in MQQMethod_I:
+            tmp = {}
+            for h1,h2 in header_map.items():
+                tmp[h2]=row[h1]
+            component_group_name,quantifier,label_type = self.parse_MQTransitionName(row['Name'])
+            tmp['FragmentSeriesNumber']=quantifier
+            tmp['LabelType']=label_type
+            tmp.update(feature_defaults)
+            FeatureXML_O.append(tmp)
+        return FeatureXML_O
+
+    @staticmethod
+    def parse_MQTransitionName(name_I):
+        """Parse MultiQuant transition name
+        
+        Args
+            name_I (str): transition name
+            
+        Returns
+            component_group_name_O (str): component_group_name
+            quantifier_O (int): quantifier = 1, qualifier = 2
+            label_type_O (str): Heavy or Light
+
+        """
+        name_lst = name_I.split('.')
+        component_group_name_O = name_lst[0]
+        quantifier_O = int(name_lst[1].split('_')[1])
+        label_type_O = name_lst[2]
+
+        return component_group_name_O,quantifier_O,label_type_O
