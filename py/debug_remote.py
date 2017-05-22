ffc1a6d61638cb08973feb1a73fcc632351da6cc
Douglas McCloskey <dmccloskey87@gmail.com>
2017-05-22 13:38:20 +0200

    updates to py code

--- py/debug_remote.py
+++ py/debug_remote.py
@@ -2,7 +2,6 @@
 ######
 import ptvsd
 ptvsd.enable_attach("my_secret", address = ('0.0.0.0', 3000))
-# ptvsd.enable_attach(None, address = ('0.0.0.0', 3000))
 #enable the below line of code only if you want the application to wait untill the debugger has attached to it
 ptvsd.wait_for_attach()
 ######
@@ -14,15 +13,15 @@ m = __main__()
 # filename='/home/user/openMS_MRMworkflow/openSWATH_cmd_params_Isolate1.csv'
 # m.run_openSWATH_cmd(filename)
 
-# # Test openSWATH_py
-# m.run_openSWATH_py(
-#     filename_filenames='/home/user/openMS_MRMworkflow/filenames.csv',
-#     filename_params='/home/user/openMS_MRMworkflow/MRMFeatureFinderScoring_params.csv',
-#     delimiter = ';'
-#     );
-
-# Test file conversions
-m.convert_MQQMethod2Feature(
-    filename_I='/home/user/openMS_MRMworkflow/qmethod.csv',
-    filename_O='/home/user/openMS_MRMworkflow/feature.csv'
+# Test openSWATH_py
+m.run_openSWATH_py(
+    filename_filenames='/home/user/openMS_MRMworkflow/filenames.csv',
+    filename_params='/home/user/openMS_MRMworkflow/MRMFeatureFinderScoring_params.csv',
+    delimiter=';'
     )
+
+# # Test file conversions
+# m.convert_MQQMethod2Feature(
+#     filename_I='/home/user/openMS_MRMworkflow/qmethod.csv',
+#     filename_O='/home/user/openMS_MRMworkflow/feature.csv'
+#     )
