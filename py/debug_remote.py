50eabb3feeba56789c72f3b401d5546539dc0aa2
Douglas McCloskey <dmccloskey87@gmail.com>
2017-05-22 11:57:54 +0200

    updates to docker-compose

--- py/debug_remote.py
+++ py/debug_remote.py
@@ -1,7 +1,7 @@
 #VSCode remote debugging
 ######
 import ptvsd
-ptvsd.enable_attach("my_secret", address = ('0.0.0.0', 3000))
+ptvsd.enable_attach("my_secret", address = ('0.0.0.0', 3001))
 # ptvsd.enable_attach(None, address = ('0.0.0.0', 3000))
 #enable the below line of code only if you want the application to wait untill the debugger has attached to it
 ptvsd.wait_for_attach()
