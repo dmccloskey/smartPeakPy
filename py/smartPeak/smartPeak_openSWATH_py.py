34618a53c49165961a074d6e868cc69232c55b61
Douglas McCloskey <dmccloskey87@gmail.com>
2017-05-23 13:33:21 +0200

    updates to py code

--- py/smartPeak/smartPeak_openSWATH_py.py
+++ py/smartPeak/smartPeak_openSWATH_py.py
@@ -67,8 +67,8 @@ class smartPeak_openSWATH_py():
         chromatograms_mapped = mrmmapper.algorithm(
             chromatogram_map=chromatograms,
             targeted=targeted, 
-            precursor_tolerance=0.001,
-            product_tolerance=0.001, 
+            precursor_tolerance=0.0005,
+            product_tolerance=0.0005, 
             allow_unmapped=True,
             allow_double_mappings=True
         )
