ecf55a6a8eeb487901bd3b5d94a56cb25813d06c
Douglas McCloskey <dmccloskey87@gmail.com>
2017-09-20 13:17:03 +0200

    [FIX] updates to docker file

--- py/debug_local.py
+++ py/debug_local.py
@@ -3,7 +3,7 @@
 from smartPeak.__main__ import __main__
 m = __main__()
 
-# Test file conversions
+# Run Quantitation
 m.convert_MQQMethod2Feature(
     filename_I='C:/Users/domccl/Dropbox (UCSD SBRG)/Project_FastPeak/openMS_MRMworkflow/qmethod.csv',
     filename_O='C:/Users/domccl/Dropbox (UCSD SBRG)/Project_FastPeak/openMS_MRMworkflow/feature.csv'
