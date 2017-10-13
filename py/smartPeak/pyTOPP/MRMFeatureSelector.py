c4d726f93a999d3173a182a7866d11bdb724408d
Douglas McCloskey <dmccloskey87@gmail.com>
2017-10-13 11:19:53 +0200

    [FIX] changed verbosity default

--- py/smartPeak/pyTOPP/MRMFeatureSelector.py
+++ py/smartPeak/pyTOPP/MRMFeatureSelector.py
@@ -210,7 +210,7 @@ class MRMFeatureSelector():
         variable_type = 'integer',
         optimal_threshold = 0.5,
         score_weights = [],
-        verbose_I=True
+        verbose_I=False
         ):
         """
         optimize the retention time using QMIP
