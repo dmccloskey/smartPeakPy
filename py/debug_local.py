028b0946b5c0271a1b8888492c42243049675b51
Douglas McCloskey <dmccloskey87@gmail.com>
2017-10-13 11:37:53 +0200

    [FIX] changed .yml

--- py/debug_local.py
+++ py/debug_local.py
@@ -1,8 +1,12 @@
-from smartPeak.__main__ import __main__
+from smartPeak.core.__main__ import __main__
 m = __main__()
 
 # Run Quantitation
 m.run_AbsoluteQuantitation_py(
     filename_filenames='/home/user/openMS_MRMworkflow/Unknowns/filenames.csv',
     filename_params='/home/user/openMS_MRMworkflow/Unknowns/MRMFeatureFinderScoring_params.csv',
-    delimiter=',',)
+    delimiter=',',
+    pick_peaks = True,
+    select_peaks = True,
+    quantify_peaks = True
+    )
