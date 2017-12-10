7f6a61f2082710f3b9a7e6184c03bfce59de9589
Douglas McCloskey <dmccloskey87@gmail.com>
2017-12-10 14:53:37 +0100

    [FEATURE] added peak and feature plotter

--- smartPeak/core/__main__.py
+++ smartPeak/core/__main__.py
@@ -29,7 +29,6 @@ class __main__():
         """
         # additional resources
         from smartPeak.pyTOPP.SequenceHandler import SequenceHandler
-        from smartPeak.pyTOPP.FeaturePlotter import FeaturePlotter
         from smartPeak.core.smartPeak_openSWATH import smartPeak_openSWATH
         from smartPeak.core.smartPeak_AbsoluteQuantitation import \
             smartPeak_AbsoluteQuantitation
@@ -40,7 +39,6 @@ class __main__():
 
         # class initializations        
         seqhandler = SequenceHandler()
-        featurePlotter = FeaturePlotter()
         AbsoluteQuantitation = smartPeak_AbsoluteQuantitation()
         openSWATH = smartPeak_openSWATH()
         smartpeak_i = smartPeak_i()
@@ -145,25 +143,23 @@ class __main__():
                         MRMFeatureFilter_filter_params_I=params[
                             'MRMFeatureFilter.filter_MRMFeatures'],
                         # qmip algorithm
-                        # MRMFeatureSelector_select_params_I=params[
-                        #     'MRMFeatureSelector.select_MRMFeatures_qmip'],
-                        # MRMFeatureSelector_schedule_params_I=params[
-                        #     'MRMFeatureSelector.schedule_MRMFeatures_qmip'],
-                        # score algorithm
                         MRMFeatureSelector_select_params_I=params[
-                            'MRMFeatureSelector.select_MRMFeatures_score'],
-                        MRMFeatureSelector_schedule_params_I={},
+                            'MRMFeatureSelector.select_MRMFeatures_qmip'],
+                        MRMFeatureSelector_schedule_params_I=params[
+                            'MRMFeatureSelector.schedule_MRMFeatures_qmip'],
+                        # score algorithm
+                        # MRMFeatureSelector_select_params_I=params[
+                        #     'MRMFeatureSelector.select_MRMFeatures_score'],
+                        # MRMFeatureSelector_schedule_params_I={},
                         verbose_I=verbose_I
                     )
                     # export diagnostic plots
                     features_pdf_o = '''%s/features/%s''' % (data_dir, sample) 
-                    featurePlotter.plot_peaks(
-                        filename_I=features_pdf_o,
-                        chromatograms=openSWATH.chromatogram_map,
-                        features=openSWATH.featureMap,
-                        plot_params={},
-                        verbose_I=verbose_I
-                    )
+                    openSWATH.export_featurePlots(
+                        filenames_I={'features_pdf_o': features_pdf_o},
+                        FeaturePlotter_params_I=params[
+                            'FeaturePlotter'],
+                        verbose_I=False)
                     # store
                     openSWATH.store_featureMap({
                         'featureXML_o': featureXML_o,
--- smartPeak/pyTOPP/FeaturePlotter.py
+++ smartPeak/pyTOPP/FeaturePlotter.py
@@ -18,16 +18,15 @@ class FeaturePlotter():
         self.MSExperiment = []
         self.featureMap = {}
 
-    def plot_peaks(
+    def plot_features(
         self,
         filename_I,
         chromatograms,       
         features,     
         plot_params=[
-            {"name": "", "value": 1}],
-        verbose_I=False
+            {"name": "", "value": 1}]
     ):
-        """Plot peaks in a .pdf file
+        """Plot peaks with annoted features in a .pdf file
         
         Args:
             filename_I (string): name of the file
@@ -35,47 +34,100 @@ class FeaturePlotter():
             featureMap (pyopenms.FeatureMap): mapped features
 
         """
-        if verbose_I:
-            print("Ploting peaks with features")
-
         # main loop
         for feature in features:
             component_group_name = feature.getMetaValue("PeptideRef").decode("utf-8")
-            pp = PdfPages(filename_I + "_" + component_group_name + ".pdf")
-            n_plots = len(feature.getSubordinates())
-            for sub_cnt, subordinate in enumerate(feature.getSubordinates()):
-                component_name = subordinate.getMetaValue("native_id")
-                chrom = [
-                    c for c in chromatograms.getChromatograms()
-                    if c.getNativeID() == component_name]
+            # n_plots = len(feature.getSubordinates())
 
-                fig = plt.figure(num=sub_cnt)
-                # ax1 = fig.add_subplot(1, n_plots, sub_cnt + 1)
-                ax1 = fig.add_subplot(1, 1, 1)
+            # open the pdf file
+            with PdfPages(filename_I + "_" + component_group_name + ".pdf") as pp:
+                for sub_cnt, subordinate in enumerate(feature.getSubordinates()):
+                    component_name = subordinate.getMetaValue("native_id")
+                    chrom = [
+                        c for c in chromatograms.getChromatograms()
+                        if c.getNativeID() == component_name]
 
-                # raw points
-                ax1.scatter(
-                    chrom[0].get_peaks()[0],
-                    chrom[0].get_peaks()[1], 
-                    s=10, c='b', marker=".", label='points'
-                    )
+                    # generate the new figure
+                    fig = plt.figure()
+                    # fig = plt.figure(num=sub_cnt)
+                    # ax1 = fig.add_subplot(1, n_plots, sub_cnt + 1)
+                    ax1 = fig.add_subplot(1, 1, 1)
 
-                # features
-                feature_rt = []
-                feature_int = []
-                for i, p in enumerate(chrom[0].get_peaks()[0]):
-                    if p >= feature.getMetaValue("leftWidth") and\
-                    p <= feature.getMetaValue("rightWidth"):
-                        feature_rt.append(p)
-                        feature_int.append(chrom[0].get_peaks()[1][i])
-                ax1.scatter(
-                    feature_rt, feature_int, s=10, c='r', 
-                    marker="o", label='selected peak')
-                
-                ax1.set_title(component_name.decode("utf-8"))
+                    # raw points
+                    ax1.scatter(
+                        chrom[0].get_peaks()[0],
+                        chrom[0].get_peaks()[1], 
+                        s=10, c='b', marker=".", label='points'
+                        )
 
-                # plt.legend(loc='upper left')
-                # fig.savefig(filename_I + "_" + component_name.decode("utf-8") + ".pdf")
-                pp.savefig(fig)
-                plt.close(fig)
-            pp.close()
+                    # features
+                    feature_rt = []
+                    feature_int = []
+                    for i, p in enumerate(chrom[0].get_peaks()[0]):
+                        if p >= feature.getMetaValue("leftWidth") and\
+                        p <= feature.getMetaValue("rightWidth"):
+                            feature_rt.append(p)
+                            feature_int.append(chrom[0].get_peaks()[1][i])
+                    ax1.scatter(
+                        feature_rt, feature_int, s=10, c='r', 
+                        marker="o", label='selected peak')
+                    
+                    ax1.set_title(component_name.decode("utf-8"))
+
+                    # plt.legend(loc='upper left')
+                    # fig.savefig(filename_I + "_" + component_name.decode("utf-8") + ".pdf")
+                    pp.savefig(fig)
+
+                    # close figures
+                    plt.close()
+                    plt.clf()
+                    plt.cla()
+
+    def plot_peaks(
+        self,
+        filename_I,
+        chromatograms,  
+        transitions,  
+        plot_params=[
+            {"name": "", "value": 1}]
+    ):
+        """Plot peaks in a .pdf file
+        
+        Args:
+            filename_I (string): name of the file
+            chromatograms (pyopenms.MSExperiment): mapped chromatograms
+            transitions (pyopenms.TargetedExperiment): list of transitions
+
+        """
+        for transition in transitions.getTransitions():
+            component_group_name = transition.getPeptideRef().decode("utf-8")
+
+            # open the pdf file
+            with PdfPages(filename_I + "_" + component_group_name + ".pdf") as pp:
+                for t in transition:
+                    component_name = t.getNativeID()
+                    chrom = [
+                        c for c in chromatograms.getChromatograms()
+                        if c.getNativeID() == component_name]
+
+                    # generate the new figure
+                    fig = plt.figure()
+                    # fig = plt.figure(num=sub_cnt)
+                    # ax1 = fig.add_subplot(1, n_plots, sub_cnt + 1)
+                    ax1 = fig.add_subplot(1, 1, 1)
+
+                    # raw points
+                    ax1.scatter(
+                        chrom[0].get_peaks()[0],
+                        chrom[0].get_peaks()[1], 
+                        s=10, c='b', marker=".", label='points'
+                        )
+                    
+                    ax1.set_title(component_name.decode("utf-8"))
+
+                    pp.savefig(fig)
+
+                    # close figures
+                    plt.close()
+                    plt.clf()
+                    plt.cla()
