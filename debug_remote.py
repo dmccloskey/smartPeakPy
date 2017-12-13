ccf209bf582e8fb8c67c4eec0bc1f6864195ebad
Douglas McCloskey <dmccloskey87@gmail.com>
2017-12-13 15:06:58 +0100

    [TESTS] all tests are passing

--- debug_remote.py
+++ debug_remote.py
@@ -17,9 +17,5 @@ m = __main__()
 #     delimiter=',',
 #     )
 
-# from tests import runAllTests
-# runAllTests()
-
-from tests.test_core_main import testMain
-test = testMain()
-test.test_core_main_LCMS_MRM()
+from tests import runAllTests
+runAllTests()
