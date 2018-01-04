d678cacee659d82164f24568f495bf2e4918ec23
Douglas McCloskey <dmccloskey87@gmail.com>
2018-01-03 19:39:11 -0800

    [TEST] updated tests for smartPeak_OpenSWATH refactor

--- tests/test_io_FileWriterOpenMS.py
+++ tests/test_io_FileWriterOpenMS.py
@@ -13,15 +13,21 @@ class TestFileWriterOpenMS():
 
     def test_store_featureMap(self):
         sampleHandler = SampleHandler()
-        sampleProcessor = SampleProcessor()
         fileReaderOpenMS = FileReaderOpenMS()
         fileWriterOpenMS = FileWriterOpenMS()        
 
         # load featureMap
         featureXML_o = '''%s/features/%s.featureXML''' % (data_dir, "test_1") 
         fileReaderOpenMS.load_featureMap(sampleHandler, {'featureXML_i': featureXML_o})
-        
-        sampleProcessor.extract_metaData(sampleHandler)
+
+        # load traML
+        traML_csv_i = '''%s%s''' % (data_dir, "traML_1.csv")
+        fileReaderOpenMS.load_TraML(sampleHandler, {'traML_csv_i': traML_csv_i})
+
+        # missing meta data in .featureXML
+        sampleHandler.meta_data = {}
+        sampleHandler.meta_data['sample_name'] = "150601_0_BloodProject01_PLT_QC_Broth-1"
+        sampleHandler.meta_data['filename'] = "/home/user/code/tests/data//mzML/mzML_1.mzML"
 
         # store
         featureXML_o = '''%s/features/%s.featureXML''' % (data_dir, "test_1") 
