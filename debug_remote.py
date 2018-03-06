c1c240329662e318377a55c3a98f80eebc164827
Douglas McCloskey <dmccloskey87@gmail.com>
2018-03-06 12:39:52 +0100

    feat: added example files for example blanks

--- debug_remote.py
+++ debug_remote.py
@@ -25,6 +25,6 @@ test = testMain()
 # test.test_main_LCMS_MRM_Unknown()
 # test.test_main_GCMS_SIM_Unknown()
 # test.test_main_HPLC_UV_Standards()  # TODO: bug in thresholds
-# test.test_main_HPLC_UV_Unknown()
-test.test_main_LCMS_MRM_Standards()  # TODO: optimize calibrator fitting
+test.test_main_HPLC_UV_Unknown()
+# test.test_main_LCMS_MRM_Standards()  # TODO: optimize calibrator fitting
 # test.test_main_LCMS_MRM_QCs()
