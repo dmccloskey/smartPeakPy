c13175d7b0b98bce42d481dde3a8e0a27be73df2
Douglas McCloskey <dmccloskey87@gmail.com>
2018-03-27 17:13:13 +0200

    unknown change

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ sequenceProcessor.createSequence(
 # 1. process all files
 raw_data_processing_methods = [
     "load_raw_data",
-    # "load_features"
-    "pick_features",
-    "filter_features",
-    "select_features",
-    "check_features",
-    "store_features", 
+    "load_features",
+    # "pick_features",
+    # "filter_features",
+    # "select_features",
+    # "check_features",
+    # "store_features", 
     # "plot_features"
 ]
 sequenceProcessor.processSequence(
@@ -46,30 +46,30 @@ sequenceProcessor.processSequence(
     raw_data_processing_methods_I=raw_data_processing_methods,
     verbose_I=True)
 
-# 2. process optimize calibrators
-sequence_segment_processing_methods = [
-    "calculate_calibration",
-    # "plot_calibrators",
-    "store_quantitation_methods",
-    # "load_quantitation_methods",
-    # "store_components_to_concentrations"
-]
-sequenceProcessor.processSequenceSegments(
-    sequenceHandler,
-    sequence_segment_processing_methods_I=sequence_segment_processing_methods,
-    verbose_I=True)
+# # 2. process optimize calibrators
+# sequence_segment_processing_methods = [
+#     "calculate_calibration",
+#     # "plot_calibrators",
+#     "store_quantitation_methods",
+#     # "load_quantitation_methods",
+#     # "store_components_to_concentrations"
+# ]
+# sequenceProcessor.processSequenceSegments(
+#     sequenceHandler,
+#     sequence_segment_processing_methods_I=sequence_segment_processing_methods,
+#     verbose_I=True)
 
-# 3. quantify standards for QC
-raw_data_processing_methods = [
-    "quantify_features",
-    "check_features",
-    "store_features", 
-    # "plot_features"
-]
-sequenceProcessor.processSequence(
-    sequenceHandler,
-    raw_data_processing_methods_I=raw_data_processing_methods,
-    verbose_I=True)
+# # 3. quantify standards for QC
+# raw_data_processing_methods = [
+#     "quantify_features",
+#     "check_features",
+#     "store_features", 
+#     # "plot_features"
+# ]
+# sequenceProcessor.processSequence(
+#     sequenceHandler,
+#     raw_data_processing_methods_I=raw_data_processing_methods,
+#     verbose_I=True)
 
 # write out a summary of all files
 sequenceSummary_csv_i = '''%s/SequenceSummary.csv''' % (dir_I)
@@ -85,7 +85,7 @@ sequenceWriter.write_dataTableFromMetaValue(
     sequenceHandler,
     filename=featureSummary_csv_i,
     meta_data=[
-        "peak_apex_int", "total_width", "width_at_50", 
+        "peak_apex_int", "RT", "total_width", "width_at_50", 
         "tailing_factor", "asymmetry_factor", "baseline_delta_2_height", 
         "points_across_baseline", "points_across_half_height", "logSN",
         "calculated_concentration",
