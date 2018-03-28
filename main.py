008c5fca1c9677ca020daef12fdfcac70473ab6c
Douglas McCloskey <dmccloskey87@gmail.com>
2018-03-28 16:31:42 +0200

    changed dirs

--- main.py
+++ main.py
@@ -1,92 +1,95 @@
-
-
 # -*- coding: utf-8 -*-
 from smartPeak.core.SequenceHandler import SequenceHandler
 from smartPeak.core.SequenceProcessor import SequenceProcessor
 from smartPeak.io.SequenceWriter import SequenceWriter
 
-sequenceHandler = SequenceHandler()
-sequenceProcessor = SequenceProcessor()
-sequenceWriter = SequenceWriter()
 
-# set the directory for all files and data
-dir_I = "/home/user/Data"
-sequenceHandler.setDirStatic(dir_I)
-sequenceHandler.setDirDynamic(dir_I)
+def main():
+    sequenceHandler = SequenceHandler()
+    sequenceProcessor = SequenceProcessor()
+    sequenceWriter = SequenceWriter()
 
-sequenceProcessor.createSequence(
-    sequenceHandler,
-    delimiter=","
-)
-        
-sequenceHandler = SequenceHandler()
-sequenceProcessor = SequenceProcessor()
-sequenceWriter = SequenceWriter()
+    # set the directory for all files and data
+    dir_I = "/home/user/Data"
+    sequenceHandler.setDirStatic(dir_I)
+    sequenceHandler.setDirDynamic(dir_I)
 
-# set the directory for all files and data
-sequenceHandler.setDirStatic(dir_I)
-sequenceHandler.setDirDynamic(dir_I)
+    sequenceProcessor.createSequence(
+        sequenceHandler,
+        delimiter=","
+    )
+            
+    sequenceHandler = SequenceHandler()
+    sequenceProcessor = SequenceProcessor()
+    sequenceWriter = SequenceWriter()
 
-sequenceProcessor.createSequence(
-    sequenceHandler,
-    delimiter=","
-)
+    # set the directory for all files and data
+    sequenceHandler.setDirStatic(dir_I)
+    sequenceHandler.setDirDynamic(dir_I)
 
-# 1. process all files
-raw_data_processing_methods = [
-    "load_raw_data",
-    # "load_features",
-    "pick_features",
-    "filter_features",
-    "select_features",
-    "check_features",
-    "store_features", 
-]
-sequenceProcessor.processSequence(
-    sequenceHandler,
-    raw_data_processing_methods_I=raw_data_processing_methods,
-    verbose_I=True)
+    sequenceProcessor.createSequence(
+        sequenceHandler,
+        delimiter=","
+    )
 
-# # 2. process optimize calibrators
-# sequence_segment_processing_methods = [
-#     "calculate_calibration",
-#     # "plot_calibrators",
-#     "store_quantitation_methods",
-#     # "store_components_to_concentrations"
-# ]
-# sequenceProcessor.processSequenceSegments(
-#     sequenceHandler,
-#     sequence_segment_processing_methods_I=sequence_segment_processing_methods,
-#     verbose_I=True)
+    # 1. process all files
+    raw_data_processing_methods = [
+        "load_raw_data",
+        # "load_features",
+        "pick_features",
+        "filter_features",
+        "select_features",
+        "check_features",
+        "store_features", 
+    ]
+    sequenceProcessor.processSequence(
+        sequenceHandler,
+        raw_data_processing_methods_I=raw_data_processing_methods,
+        verbose_I=True)
 
-# 3. quantify standards for QC
-raw_data_processing_methods = [
-    "quantify_features",
-    "check_features",
-    "store_features", 
-    # "plot_features"
-]
-sequenceProcessor.processSequence(
-    sequenceHandler,
-    raw_data_processing_methods_I=raw_data_processing_methods,
-    verbose_I=True)
+    # # 2. process optimize calibrators
+    # sequence_segment_processing_methods = [
+    #     "calculate_calibration",
+    #     # "plot_calibrators",
+    #     "store_quantitation_methods",
+    #     # "store_components_to_concentrations"
+    # ]
+    # sequenceProcessor.processSequenceSegments(
+    #     sequenceHandler,
+    #     sequence_segment_processing_methods_I=sequence_segment_processing_methods,
+    #     verbose_I=True)
 
-# write out a summary of all files
-sequenceSummary_csv_i = '''%s/SequenceSummary.csv''' % (dir_I)
-sequenceWriter.write_dataMatrixFromMetaValue(
-    sequenceHandler,
-    filename=sequenceSummary_csv_i,
-    meta_data=['calculated_concentration', 'RT'],
-    sample_types=['Standard', 'Unknown']
-)
+    # 3. quantify standards for QC
+    raw_data_processing_methods = [
+        "quantify_features",
+        "check_features",
+        "store_features", 
+        # "plot_features"
+    ]
+    sequenceProcessor.processSequence(
+        sequenceHandler,
+        raw_data_processing_methods_I=raw_data_processing_methods,
+        verbose_I=True)
 
-featureSummary_csv_i = '''%s/FeatureSummary.csv''' % (dir_I)
-sequenceWriter.write_dataTableFromMetaValue(
-    sequenceHandler,
-    filename=featureSummary_csv_i,
-    meta_data=[
-        "peak_apex_int", 'RT', "total_width", "width_at_50", 
-        "tailing_factor", "asymmetry_factor", "baseline_delta_2_height", 
-        "points_across_baseline", "points_across_half_height", "logSN",
-        "QC_transition_message", "QC_transition_score", "calculated_concentration"],
-    sample_types=['Unknown', 'Standard']
+    # write out a summary of all files
+    sequenceSummary_csv_i = '''%s/SequenceSummary.csv''' % (dir_I)
+    sequenceWriter.write_dataMatrixFromMetaValue(
+        sequenceHandler,
+        filename=sequenceSummary_csv_i,
+        meta_data=['calculated_concentration', 'RT'],
+        sample_types=['Standard', 'Unknown']
+    )
+
+    featureSummary_csv_i = '''%s/FeatureSummary.csv''' % (dir_I)
+    sequenceWriter.write_dataTableFromMetaValue(
+        sequenceHandler,
+        filename=featureSummary_csv_i,
+        meta_data=[
+            "peak_apex_int", 'RT', "total_width", "width_at_50", 
+            "tailing_factor", "asymmetry_factor", "baseline_delta_2_height", 
+            "points_across_baseline", "points_across_half_height", "logSN",
+            "QC_transition_message", "QC_transition_score", "calculated_concentration"],
+        sample_types=['Unknown', 'Standard']
+
+if __name__ == "__main__":
+    main()
