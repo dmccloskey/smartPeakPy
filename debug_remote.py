a708a3a71d7715d67a116a81c3ac28393fa6704f
Douglas McCloskey <dmccloskey87@gmail.com>
2018-07-27 15:33:26 +0200

    tests: all tests pass

--- debug_remote.py
+++ debug_remote.py
@@ -1,90 +1,25 @@
-# # VSCode remote debugging
-# ######
-# import ptvsd
-# ptvsd.enable_attach("my_secret", address=('0.0.0.0', 3000))
-# # enable the below line of code only if you want the application to wait
-# # until the debugger has attached to it
-# ptvsd.wait_for_attach()
-# ######
+# VSCode remote debugging
+######
+import ptvsd
+ptvsd.enable_attach("my_secret", address=('0.0.0.0', 3000))
+# enable the below line of code only if you want the application to wait
+# until the debugger has attached to it
+ptvsd.wait_for_attach()
+######
 
-# -*- coding: utf-8 -*-
-from smartPeak.core.SequenceHandler import SequenceHandler
-from smartPeak.core.SequenceProcessor import SequenceProcessor
-from smartPeak.io.SequenceWriter import SequenceWriter
+from smartPeak.__main__ import __main__
+m = __main__()
 
-sequenceHandler = SequenceHandler()
-sequenceProcessor = SequenceProcessor()
-sequenceWriter = SequenceWriter()
+# # GCMS_fullScan
+# m.main(
+#     filename_filenames='/home/user/Data/GCMS_fullScan/filenames.csv',
+#     filename_params='/home/user/Data/GCMS_fullScan/MRMFeatureFinderScoring_params.csv',
+#     delimiter=',',
+#     )
 
-# set the directory for all files and data
-dir_I = "/home/user/Data"
-sequenceHandler.setDirStatic(dir_I)
-sequenceHandler.setDirDynamic(dir_I)
+from tests import runAllTests
+runAllTests()
 
-sequenceProcessor.createSequence(
-    sequenceHandler,
-    delimiter=","
-)
-
-# # 1. process and store all files
-# raw_data_processing_methods = [
-#     "load_raw_data",
-#     # "load_features",
-#     "pick_features",
-#     "quantify_features",
-#     "check_features",
-#     "store_features",
-# ]
-# sequenceProcessor.processSequence(
-#     sequenceHandler,
-#     raw_data_processing_methods_I=raw_data_processing_methods,
-#     verbose_I=True)
-
-# # 2. filter and select
-# raw_data_processing_methods = [
-#     "clear_feature_history",
-#     "load_features",
-#     "save_features",
-#     "filter_features",
-#     "select_features",
-#     "annotate_used_features",
-#     "store_features",
-# ]
-# sequenceProcessor.processSequence(
-#     sequenceHandler,
-#     raw_data_processing_methods_I=raw_data_processing_methods,
-#     verbose_I=True)
-
-# 3. report and plot only
-raw_data_processing_methods = [
-    "load_raw_data",
-    "load_features",
-    # "plot_features"
-]
-sequenceProcessor.processSequence(
-    sequenceHandler,
-    raw_data_processing_methods_I=raw_data_processing_methods,
-    verbose_I=True)
-
-# write out a summary of all files
-sequenceSummary_csv_i = '''%s/SequenceSummary.csv''' % (dir_I)
-sequenceWriter.write_dataMatrixFromMetaValue(
-    sequenceHandler,
-    filename=sequenceSummary_csv_i,
-    meta_data=['calculated_concentration'],
-    sample_types=['Unknown']
-)
-
-featureSummary_csv_i = '''%s/FeatureSummary.csv''' % (dir_I)
-sequenceWriter.write_dataTableFromMetaValue(
-    sequenceHandler,
-    filename=featureSummary_csv_i,
-    meta_data=[
-        "used_", "RT", "peak_apex_int", "total_width", "width_at_50", 
-        "tailing_factor", "asymmetry_factor", "baseline_delta_2_height", 
-        "points_across_baseline", "points_across_half_height", "logSN",
-        "calculated_concentration",
-        "QC_transition_message", "QC_transition_pass", "QC_transition_score",
-        "QC_transition_group_message", "QC_transition_group_score"],
-    sample_types=['Unknown']
-)
+# from tests.test_core_RawDataProcessor import TestRawDataProcessor
+# test = TestRawDataProcessor()
+# test.test_annotateUsedFeatures()
--- main.py
+++ main.py
@@ -17,22 +17,45 @@ sequenceProcessor.createSequence(
     delimiter=","
 )
 
-# process and store all files
+# 1. process and store all files
 raw_data_processing_methods = [
     "load_raw_data",
     # "load_features",
     "pick_features",
-    "filter_features",
-    "select_features",
-    # "validate_features",
     "quantify_features",
     "check_features",
     "store_features",
-    "plot_features"
 ]
 sequenceProcessor.processSequence(
     sequenceHandler,
-    raw_data_processing_methods_I=raw_data_processing_methods)
+    raw_data_processing_methods_I=raw_data_processing_methods,
+    verbose_I=True)
+
+# 2. filter and select
+raw_data_processing_methods = [
+    "clear_feature_history",
+    "load_features",
+    "save_features",
+    "filter_features",
+    "select_features",
+    "annotate_used_features",
+    "store_features",
+]
+sequenceProcessor.processSequence(
+    sequenceHandler,
+    raw_data_processing_methods_I=raw_data_processing_methods,
+    verbose_I=True)
+
+# 3. report and plot only
+raw_data_processing_methods = [
+    "load_raw_data",
+    "load_features",
+    # "plot_features"
+]
+sequenceProcessor.processSequence(
+    sequenceHandler,
+    raw_data_processing_methods_I=raw_data_processing_methods,
+    verbose_I=True)
 
 # write out a summary of all files
 sequenceSummary_csv_i = '''%s/SequenceSummary.csv''' % (dir_I)
@@ -40,5 +63,19 @@ sequenceWriter.write_dataMatrixFromMetaValue(
     sequenceHandler,
     filename=sequenceSummary_csv_i,
     meta_data=['calculated_concentration'],
+    sample_types=['Unknown']
+)
+
+featureSummary_csv_i = '''%s/FeatureSummary.csv''' % (dir_I)
+sequenceWriter.write_dataTableFromMetaValue(
+    sequenceHandler,
+    filename=featureSummary_csv_i,
+    meta_data=[
+        "used_", "RT", "peak_apex_int", "total_width", "width_at_50", 
+        "tailing_factor", "asymmetry_factor", "baseline_delta_2_height", 
+        "points_across_baseline", "points_across_half_height", "logSN",
+        "calculated_concentration",
+        "QC_transition_message", "QC_transition_pass", "QC_transition_score",
+        "QC_transition_group_message", "QC_transition_group_score"],
     sample_types=['Unknown']
 )
